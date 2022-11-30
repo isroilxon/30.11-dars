00e093238ccc491248401a9ce0e880ed1a7bef95
isroil <5085624@mail.ru>
2022-11-30 06:52:25 +0500

    Initial Commit

---
+++ 30.11 dars/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  30.11 dars
+//
+//  Created by mac on 30/11/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func button(_ sender: Any) {
+        title = "title"
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 30.11 dars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  30.11 dars
+//
+//  Created by mac on 30/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
